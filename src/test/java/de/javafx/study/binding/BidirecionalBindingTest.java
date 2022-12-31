986beecf3a26cd46ed78ae48b0ab4ef48df3e77c
Phillipe Lemos <phillipe.lemos@gmail.com>
2022-12-31 22:07:07 +0100

    Added bidirecional binding testing

--- src/test/java/de/javafx/study/binding/BidirecionalBindingTest.java
+++ src/test/java/de/javafx/study/binding/BidirecionalBindingTest.java
@@ -1,4 +1,51 @@
 package de.javafx.study.binding;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class BidirecionalBindingTest {
+
+    private IntegerProperty x;
+
+    private IntegerProperty y;
+
+    private IntegerProperty z;
+
+    @BeforeEach
+    void setUp() {
+        x = new SimpleIntegerProperty(1);
+        y = new SimpleIntegerProperty(2);
+        z = new SimpleIntegerProperty(3);
+    }
+
+    @Test
+    void checkingValuesBeforeBinding() {
+        assertThat(x.get(), equalTo(1));
+        assertThat(y.get(), equalTo(2));
+        assertThat(z.get(), equalTo(3));
+    }
+
+
+    @Test
+    void checkingBindingXAndY() {
+        x.bindBidirectional(y);
+        assertThat(x.get(), equalTo(2));
+        assertThat(y.get(), equalTo(2));
+        x.unbindBidirectional(y);
+    }
+
+    @Test
+    void afterBidingTheLastOneAllShouldBeEqual() {
+        x.bindBidirectional(y);
+        x.bindBidirectional(z);
+        assertThat(x.get(), equalTo(3));
+        assertThat(y.get(), equalTo(3));
+        assertThat(z.get(), equalTo(3));
+    }
+
 }
